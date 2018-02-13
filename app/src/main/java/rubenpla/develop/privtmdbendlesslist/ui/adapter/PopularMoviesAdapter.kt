5786fdf4ebdfacbcca93c734b488e4994c53d156
Ruben Pla <ruben.pla@alten.es>
2018-02-13 14:16:57 +0100

    - [BIND/MODEL] Created ProgressbarBindModel model for binding purposes - [LAYOUT] Added binding to movie_item_progress.xml layout file 		- [FIX] Changed bind variable name for movie_item_list.xml layout file  'repo' ---> 'movie' - [UI/ADAPTER] Added ViewHolder's internal classes for MovieRepoAdapter

--- app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/PopularMoviesAdapter.kt
+++ app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/PopularMoviesAdapter.kt
@@ -1,9 +1,13 @@
 package rubenpla.develop.privtmdbendlesslist.ui.adapter
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
 import rubenpla.develop.privtmdbendlesslist.bind.model.MovieBindModel
+import rubenpla.develop.privtmdbendlesslist.databinding.MovieItemListBinding
+import rubenpla.develop.privtmdbendlesslist.databinding.MovieItemProgressBinding
 import rubenpla.develop.privtmdbendlesslist.ui.adapter.base.BaseMoviesAdapter
 
 /**
@@ -26,5 +30,23 @@ class PopularMoviesAdapter (val context : Context, val list : MutableList<MovieB
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int, payloads: MutableList<Any>?) {
         super.onBindViewHolder(holder, position, payloads)
+    }
+
+    internal  class MovieViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        private val binding : MovieItemListBinding = DataBindingUtil.bind(itemView)
+
+        fun bind(movie : MovieBindModel, listener : (MovieBindModel?) -> Unit) {
+            binding.movie = movie
+            itemView.setOnClickListener { listener(movie) }
+        }
+    }
+
+    internal class ProgressbarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding : MovieItemProgressBinding = DataBindingUtil.bind(itemView)
+
+        fun bind (isIndeterminate : Boolean) {
+            binding.movieItemProgressProgressbar.isIndeterminate = isIndeterminate
+        }
+
     }
 }
