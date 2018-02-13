bdd1b10392996a2cf95156bc838006e8ca1070e6
Ruben Pla <ruben.pla@alten.es>
2018-02-13 15:03:42 +0100

    - [UI/ADAPTER] - Returning corresponding view oon 'OnCreateViewHolder()' method 			    - Binding holder corresponding to its view type (movie/progressbar)

--- app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/PopularMoviesAdapter.kt
+++ app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/PopularMoviesAdapter.kt
@@ -3,8 +3,10 @@ package rubenpla.develop.privtmdbendlesslist.ui.adapter
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import rubenpla.develop.privtmdbendlesslist.R
 import rubenpla.develop.privtmdbendlesslist.bind.model.MovieBindModel
 import rubenpla.develop.privtmdbendlesslist.databinding.MovieItemListBinding
 import rubenpla.develop.privtmdbendlesslist.databinding.MovieItemProgressBinding
@@ -13,29 +15,30 @@ import rubenpla.develop.privtmdbendlesslist.ui.adapter.base.BaseMoviesAdapter
 /**
  * Created by alten on 13/2/18.
  */
-class PopularMoviesAdapter (val context : Context, val list : MutableList<MovieBindModel?>)
+class PopularMoviesAdapter (private val context : Context,
+                            private val list : MutableList<MovieBindModel?>,
+                            private val listener : (MovieBindModel?) -> Unit)
     : BaseMoviesAdapter(context, list) {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        return super.onCreateViewHolder(parent, viewType)
-    }
-
-    override fun getItemCount(): Int {
-        return super.getItemCount()
+        return if (viewType == -1)
+                    ProgressbarViewHolder(LayoutInflater.from(context)
+                            .inflate(R.layout.movie_item_progress, parent, false))
+               else
+                    MovieViewHolder(LayoutInflater.from(context).inflate(R.layout.movie_item_list,
+                            parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        super.onBindViewHolder(holder, position)
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int, payloads: MutableList<Any>?) {
-        super.onBindViewHolder(holder, position, payloads)
+        if (holder is MovieViewHolder)
+            holder.bind(list[position], listener)
+        else (holder as? ProgressbarViewHolder)?.bind(true)
     }
 
     internal  class MovieViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         private val binding : MovieItemListBinding = DataBindingUtil.bind(itemView)
 
-        fun bind(movie : MovieBindModel, listener : (MovieBindModel?) -> Unit) {
+        fun bind(movie : MovieBindModel?, listener : (MovieBindModel?) -> Unit) {
             binding.movie = movie
             itemView.setOnClickListener { listener(movie) }
         }
@@ -47,6 +50,5 @@ class PopularMoviesAdapter (val context : Context, val list : MutableList<MovieB
         fun bind (isIndeterminate : Boolean) {
             binding.movieItemProgressProgressbar.isIndeterminate = isIndeterminate
         }
-
     }
 }
--- app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/base/BaseMoviesAdapter.kt
+++ app/src/main/java/rubenpla/develop/privtmdbendlesslist/ui/adapter/base/BaseMoviesAdapter.kt
@@ -8,13 +8,16 @@ import rubenpla.develop.privtmdbendlesslist.bind.model.MovieBindModel
 /**
  * Created by alten on 13/2/18.
  */
-open class BaseMoviesAdapter(private  val context : Context, private val list : MutableList<MovieBindModel?>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+open class BaseMoviesAdapter(private  val context :
+                             Context, private val list : MutableList<MovieBindModel?>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
