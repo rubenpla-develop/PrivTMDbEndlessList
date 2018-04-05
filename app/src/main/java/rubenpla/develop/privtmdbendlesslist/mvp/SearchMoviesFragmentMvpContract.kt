a64243df6b9cd484cf912ac7078c217d10424498
Ruben Pla <rubenpla.develop@gmail.com>
2018-04-05 17:30:59 +0200

    - [MVP] Created SearMoviesFragmentMVpContract contract's class.              - Implemented basic mvp structure on SearchMoviesFragment & SearchMoviesFragmentPresenterImpl

---
+++ app/src/main/java/rubenpla/develop/privtmdbendlesslist/mvp/SearchMoviesFragmentMvpContract.kt
@@ -0,0 +1,21 @@
+package rubenpla.develop.privtmdbendlesslist.mvp
+
+import io.reactivex.Flowable
+import rubenpla.develop.privtmdbendlesslist.data.model.MoviesResponse
+import rubenpla.develop.privtmdbendlesslist.data.model.MoviesResultsItem
+import rubenpla.develop.privtmdbendlesslist.mvp.base.BasePresenter
+import rubenpla.develop.privtmdbendlesslist.mvp.base.BaseView
+
+class SearchMoviesFragmentMvpContract {
+
+    interface SearchMoviesFragmentView : BaseView<SearchMoviesFragmentPresenter> {
+        fun showSelectedMovie(item : MoviesResultsItem?)
+        fun showProgress(): Boolean
+        fun hideProgress(): Boolean
+    }
+
+    interface SearchMoviesFragmentPresenter : BasePresenter {
+        fun getSearchResults(page: Int): Flowable<MoviesResponse>?
+        fun onLoadMore(page : Int)
+    }
+}
