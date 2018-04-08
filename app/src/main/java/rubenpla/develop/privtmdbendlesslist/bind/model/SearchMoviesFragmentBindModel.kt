3c8b9f09c33b6293cd93c8850af8bee5ecfd77ac
Ruben Pla <rubenpla.develop@gmail.com>
2018-04-08 13:04:13 +0200

    - [BINDING]Added missing SearchMOviesFragmentBindModel class

---
+++ app/src/main/java/rubenpla/develop/privtmdbendlesslist/bind/model/SearchMoviesFragmentBindModel.kt
@@ -0,0 +1,21 @@
+package rubenpla.develop.privtmdbendlesslist.bind.model
+
+import android.databinding.BaseObservable
+import android.databinding.Bindable
+import rubenpla.develop.privtmdbendlesslist.BR
+
+class SearchMoviesFragmentBindModel : BaseObservable() {
+
+    @get:Bindable
+    var resetLoading: Boolean = false
+        set(resetLoading) {
+            field = resetLoading
+            notifyPropertyChanged(BR.resetLoading)
+        }
+    @get:Bindable
+    var visibleThreshold: Int = 0
+        set(visibleThreshold) {
+            field = visibleThreshold
+            notifyPropertyChanged(BR.visibleThreshold)
+        }
+}
